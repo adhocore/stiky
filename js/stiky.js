db9d89194dcf4490e979444c52a1cba654fe85c4
Jitendra Adhikari <jiten.adhikary@gmail.com>
2016-10-06 19:50:29 +0700

    use vendor and worker

--- js/stiky.js
+++ js/stiky.js
@@ -1,18 +1,18 @@
-// Configure module paths for require.js
+// Configure module paths for require
 require.config({
   paths: {
-    // From CDN
-    jquery    : "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min",
-    underscore: "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min",
-    backbone  : "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min",
-    localstore: "https://cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.1.16/backbone.localStorage-min",
+    // From Vendors
+    jquery    : 'vendor/jquery.min',
+    underscore: 'vendor/underscore.min',
+    backbone  : 'vendor/backbone.min',
+    localstore: 'vendor/backbone.localstorage.min',
 
     // Our own
-    data      : "lib/data",
-    stikyapp  : "lib/stikyapp",
-    note      : "lib/model/stikynote",
-    notes     : "lib/collection/stikynotes",
-    noteview  : "lib/view/noteview"
+    data      : 'lib/data',
+    stikyapp  : 'lib/stikyapp',
+    note      : 'lib/model/stikynote',
+    notes     : 'lib/collection/stikynotes',
+    noteview  : 'lib/view/noteview'
   }
 });
 
---
+++ js/sw.js
@@ -0,0 +1,72 @@
+var cacheName = 'stiky-v1';
+var urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/css/stiky.css',
+  '/js/sw.js',
+  '/js/stiky.js',
+  '/js/lib/stikyapp.js',
+  '/js/lib/collection/stikynotes.js',
+  '/js/lib/view/noteview.js',
+  '/js/lib/model/stikynote.js',
+  '/js/vendor/require.min.js',
+  '/js/vendor/jquery.min.js',
+  '/js/vendor/underscore.min.js',
+  '/js/vendor/backbone.min.js',
+  '/js/vendor/backbone.localstorage.min.js',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['stiky-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(cacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
